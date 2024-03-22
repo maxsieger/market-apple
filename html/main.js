269c4cf0aba5dcf2f45ac30306f28eaf8e056f66
maxsieger <maksnov10@mail.ru>
2024-03-22 16:25:43 +0300

    top

--- html/main.js
+++ html/main.js
@@ -1,1 +1,101 @@
 
+const iPhone = document.querySelector(".iPhone")
+
+const samsung = document.querySelector(".samsung")
+
+iPhone.addEventListener("click", function(event) {
+    let clickTarget = event.target;
+    let activeButton = iPhone.querySelector(".active");
+    let priceItem = iPhone.querySelector(".price");
+    let currentPrice;
+
+    let activeButtonColor = iPhone.querySelector(".active-border")
+
+    if (clickTarget.classList.contains("color-btn") && 
+    !clickTarget.classList.contains("active-border")
+    ) {
+
+            clickTarget.classList.add("active-border");
+
+            activeButtonColor.classList.remove("active-border");
+
+    }
+
+    if (clickTarget.classList.contain("btn") && !clickTarget.classList.contains("active")) {
+        clickTarget.classList.add("active");
+        activeButton.classList.remove("active");
+    }
+
+    if (clickTarget.classList.contains("btn") && !clickTarget.classList.contains("active")) {
+    clickTargetTarget.classList.add("active");
+    activeButton.classList.remove("active");
+    }
+
+    currentPrice = clickTarget.getAttribute("data-size");
+
+    if(currentPrice === "medium") {
+        priceItem.textContent = "Цена 80 000р."
+    }
+
+    if(currentPrice === "large") {
+        priceItem.textContent = "Цена 100 000р."
+    }
+
+
+});
+
+samsung.addEventListener("click", function(event) {
+    let clickTarget = event.target;
+    let activeButton = iPhone.querySelector(".active");
+    let priceItem = iPhone.querySelector(".price");
+    let currentPrice;
+
+    let activeButtonColor = iPhone.querySelector(".active-border")
+
+    if (clickTarget.classList.contains("color-btn") && 
+    !clickTarget.classList.contains("active-border")
+    ) {
+
+            clickTarget.classList.add("active-border");
+
+            activeButtonColor.classList.remove("active-border");
+
+    }
+
+    if (clickTarget.classList.contain("btn") && !clickTarget.classList.contains("active")) {
+        clickTarget.classList.add("active");
+        activeButton.classList.remove("active");
+    }
+
+    if (clickTarget.classList.contains("btn") && !clickTarget.classList.contains("active")) {
+    clickTargetTarget.classList.add("active");
+    activeButton.classList.remove("active");
+    }
+
+    currentPrice = clickTarget.getAttribute("data-size");
+
+    if(currentPrice === "medium") {
+        priceItem.textContent = "Цена 80 000р."
+    }
+
+    if(currentPrice === "large") {
+        priceItem.textContent = "Цена 100 000р."
+    }
+});
+
+const buy = document.querySelector(".buy")
+const buy_1 = document.querySelector(".buy_1")
+buy.addEventListener("click", active_prompt)
+buy_1.addEventListener("click", active_prompt)
+
+function active_prompt () {
+    let ptmpt = document.querySelector(".prompt")
+    let otvet = prompt("Введите пин код карточки привязанной к вашему аккаунту:")
+    if ( otvet === "2262" ) {
+        ptmpt.classList.add("active_prompt")
+    } else { 
+        ptmpt.classList.add("active_prompt_2")
+        ptmpt.textContent = "Неверный пинкод"
+    }
+}
+
